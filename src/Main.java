073fbb2030a524103452d00733ce6fdacbc6ba6d
Virus <Virus@DESKTOP-AMIM2LS>
2018-06-10 03:24:10 -0500

    Primer commit al programa de dibujo de pelotas

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jesus Ivan
+ */
+
+public class Main {
+    public static void main(String args[]){
+        new Pelotas20();
+    }
+}
